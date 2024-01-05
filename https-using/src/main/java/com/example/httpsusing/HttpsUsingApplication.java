d8c076a28a249d91db66697df0d6b3afb6872c03
chamith <erandachamith322@gmail.com>
2024-01-05 11:19:50 +0530

    HTTPS practice code

---
+++ https-using/src/main/java/com/example/httpsusing/HttpsUsingApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpsusing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsUsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsUsingApplication.class, args);
+	}
+
+}
---
+++ https-using/src/test/java/com/example/httpsusing/HttpsUsingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpsusing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsUsingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
